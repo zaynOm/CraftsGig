feb70cdc77fbb3519144febfa9628ed07f7b0431
zaynOm <ouaziz0626@gmail.com>
2024-03-12 14:16:11 +0000

    fix deployment issues

--- backend/wsgi.py
+++ backend/wsgi.py
@@ -2,11 +2,4 @@ from app import create_app
 
 
 if __name__ == '__main__':
-    import gunicorn
     app = create_app()
-
-    gunicorn.gunicorn.serve(
-        'wsgi:app',
-        workers=4,
-        bind='0.0.0.0:5000'
-    )
