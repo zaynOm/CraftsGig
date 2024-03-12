f48f6da3c745d22e7fee72553e183a039763ea9a
zaynOm <ouaziz0626@gmail.com>
2024-03-12 14:26:27 +0000

    fix deployment issues

--- backend/app.py
+++ backend/app.py
@@ -17,102 +17,99 @@ from routes.auth import blp as AuthBlueprint
 
 load_dotenv()
 
-def create_app():
 
-    app = Flask(__name__)
-    app.config['API_TITLE'] = 'CraftsMan'
-    app.config['API_VERSION'] = 'v1'
-    app.config["OPENAPI_VERSION"] = "3.0.2"
-    app.config['SQLALCHEMY_DATABASE_URI'] = environ.get('DATABASE_URL')
+app = Flask(__name__)
+app.config['API_TITLE'] = 'CraftsMan'
+app.config['API_VERSION'] = 'v1'
+app.config["OPENAPI_VERSION"] = "3.0.2"
+app.config['SQLALCHEMY_DATABASE_URI'] = environ.get('DATABASE_URL')
 
-    app.config["OPENAPI_VERSION"] = "3.0.3"
-    app.config["OPENAPI_URL_PREFIX"] = "/"
-    app.config["OPENAPI_SWAGGER_UI_PATH"] = "/docs"
-    app.config["OPENAPI_SWAGGER_UI_URL"] = "https://cdn.jsdelivr.net/npm/swagger-ui-dist/"
+app.config["OPENAPI_VERSION"] = "3.0.3"
+app.config["OPENAPI_URL_PREFIX"] = "/"
+app.config["OPENAPI_SWAGGER_UI_PATH"] = "/docs"
+app.config["OPENAPI_SWAGGER_UI_URL"] = "https://cdn.jsdelivr.net/npm/swagger-ui-dist/"
 
-    app.config["JWT_SECRET_KEY"] = "test_key"
-    
-
-    app.json.sort_keys = False
-    CORS(app, resources={r"/*": {"origins": "*"}})
-    init_db(app)
-    migrate = Migrate(app, db)
-    api = Api(app)
-    jwt = JWTManager(app)
-
-    api.spec.components.security_scheme(
-    "bearerAuth", {"type":"http", "scheme": "bearer", "bearerFormat": "JWT"}
-    )
-
-    create_tables(app)
-
-    # register Blueprints here
-    api.register_blueprint(AuthBlueprint)
-    api.register_blueprint(UserBlueprint)
-    api.register_blueprint(GigBlueprint)
-    api.register_blueprint(CityBlueprint)
-    api.register_blueprint(DomainBlueprint)
-    api.register_blueprint(ReviewBlueprint)
-    api.register_blueprint(FilterBlueprint)
+app.config["JWT_SECRET_KEY"] = "test_key"
 
 
+app.json.sort_keys = False
+CORS(app, resources={r"/*": {"origins": "*"}})
+init_db(app)
+migrate = Migrate(app, db)
+api = Api(app)
+jwt = JWTManager(app)
 
-    return app
+api.spec.components.security_scheme(
+"bearerAuth", {"type":"http", "scheme": "bearer", "bearerFormat": "JWT"}
+)
 
+create_tables(app)
 
-    #api.spec.options["security"] = [{"bearerAuth": []}]
-    
-    # @jwt.additional_claims_loader
-    # def add_claims_to_jwt(identity):
-    #     # TODO: Read from a config file instead of hard-coding
-    #     if identity == 1:
-    #         return {"is_admin": True}
-    #     return {"is_admin": False}
-
-
-    # @jwt.expired_token_loader
-    # def expired_token_callback(jwt_header, jwt_payload):
-    #     return jsonify({"message": "The token has expired.", "error": "token_expired"}), 401
-
-
-    # @jwt.invalid_token_loader
-    # def invalid_token_callback(error):
-    #     return (
-    #         jsonify(
-    #             {"message": "Signature verification failed.", "error": "invalid_token"}
-    #         ),
-    #         401,
-    #     )
+# register Blueprints here
+api.register_blueprint(AuthBlueprint)
+api.register_blueprint(UserBlueprint)
+api.register_blueprint(GigBlueprint)
+api.register_blueprint(CityBlueprint)
+api.register_blueprint(DomainBlueprint)
+api.register_blueprint(ReviewBlueprint)
+api.register_blueprint(FilterBlueprint)
 
 
-    # @jwt.unauthorized_loader
-    # def missing_token_callback(error):
-    #     return (
-    #         jsonify(
-    #             {
-    #                 "description": "Request does not contain an access token.",
-    #                 "error": "authorization_required",
-    #             }
-    #         ),
-    #         401,
-    #     )
 
 
-    # @jwt.needs_fresh_token_loader
-    # def token_not_fresh_callback(jwt_header, jwt_payload):
-    #     return (
-    #         jsonify(
-    #             {"description": "The token is not fresh.", "error": "fresh_token_required"}
-    #         ),
-    #         401,
-    #     )
+#api.spec.options["security"] = [{"bearerAuth": []}]
+
+# @jwt.additional_claims_loader
+# def add_claims_to_jwt(identity):
+#     # TODO: Read from a config file instead of hard-coding
+#     if identity == 1:
+#         return {"is_admin": True}
+#     return {"is_admin": False}
 
 
-    # @jwt.revoked_token_loader
-    # def revoked_token_callback(jwt_header, jwt_payload):
-    #     return (
-    #         jsonify(
-    #             {"description": "The token has been revoked.", "error": "token_revoked"}
-    #         ),
-    #         401,
-    #     )
+# @jwt.expired_token_loader
+# def expired_token_callback(jwt_header, jwt_payload):
+#     return jsonify({"message": "The token has expired.", "error": "token_expired"}), 401
+
+
+# @jwt.invalid_token_loader
+# def invalid_token_callback(error):
+#     return (
+#         jsonify(
+#             {"message": "Signature verification failed.", "error": "invalid_token"}
+#         ),
+#         401,
+#     )
+
+
+# @jwt.unauthorized_loader
+# def missing_token_callback(error):
+#     return (
+#         jsonify(
+#             {
+#                 "description": "Request does not contain an access token.",
+#                 "error": "authorization_required",
+#             }
+#         ),
+#         401,
+#     )
+
+
+# @jwt.needs_fresh_token_loader
+# def token_not_fresh_callback(jwt_header, jwt_payload):
+#     return (
+#         jsonify(
+#             {"description": "The token is not fresh.", "error": "fresh_token_required"}
+#         ),
+#         401,
+#     )
+
+
+# @jwt.revoked_token_loader
+# def revoked_token_callback(jwt_header, jwt_payload):
+#     return (
+#         jsonify(
+#             {"description": "The token has been revoked.", "error": "token_revoked"}
+#         ),
+#         401,
+#     )
