b91c503b12068b52b3e1eac7e59c488248d32a2b
zaynOm <ouaziz0626@gmail.com>
2024-03-14 22:05:21 +0000

    Use different API url for dev and prod

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -7,7 +7,7 @@ export default defineConfig({
   server: {
     proxy: {
       "/api": {
-        target: "http://localhost:5000",
+        target: import.meta.env.VITE_API_URL,
         changeOrigin: true,
         rewrite: (path) => path.replace(/^\/api/, ""),
       },
