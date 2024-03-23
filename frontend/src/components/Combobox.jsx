14cf4e81d47c61f7480dab3c68decab5b1a0cabd
zaynOm <ouaziz0626@gmail.com>
2024-03-23 16:59:30 +0000

    css improvements

--- frontend/src/components/Combobox.jsx
+++ frontend/src/components/Combobox.jsx
@@ -48,10 +48,10 @@ export function Combobox({ filterKey, data, handleChange }) {
           className="w-40 justify-between"
         >
           {value ? value : `Select ${filterKey}...`}
-          <ChevronsUpDown className="ml-2 h-4 w-4 shrink-0 opacity-50" />
+          <ChevronsUpDown className="ml-2 size-4 shrink-0 opacity-50" />
         </Button>
       </PopoverTrigger>
-      <PopoverContent className=" w-[200px] p-0">
+      <PopoverContent className="w-[200px] p-0">
         <Command>
           <CommandInput placeholder={`Search ${filterKey}...`} />
           <ScrollArea className="h-52">
